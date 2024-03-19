9efc9b3349ca5f7744082459f066a08d0b9e550a
Sarang <hangpfm0518@ewhain.net>
2024-03-20 01:08:20 +0900

    Initial setting

---
+++ DB2024Team00/src/DB2024Team00_main.java
@@ -0,0 +1,4 @@
+public class DB2024Team00_main {
+    public static void main(String[] args) {
+    }
+}
--- ESKDB/src/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("eunseo ko의 java 프로젝트");
-    }
-}
