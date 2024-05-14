7716b4a61b5101a03aacee8a9df906293d7004d3
s2eojeong <s2eojeong@gmail.com>
2024-05-14 23:56:12 +0900

    Edit MainPage

--- src/DB2024TEAM07_MainPage.java
+++ src/DB2024TEAM07_MainPage.java
@@ -5,10 +5,10 @@ public class DB2024TEAM07_MainPage extends JFrame{
     public DB2024TEAM07_MainPage(){
         setTitle("E-MATEASY");
 
-        JButton branchBtn = new JButton("회원 로그인");
-        JButton registerBtn = new JButton ("회원가입");
-        JButton memberBtn = new JButton("관리자 로그인");
-        JButton trainerBtn = new JButton("프로그램 종료");
+        JButton memberloginBtn = new JButton("회원 로그인");
+        JButton joinBtn = new JButton ("회원가입");
+        JButton adminloginBtn = new JButton("관리자 로그인");
+        JButton exitBtn = new JButton("프로그램 종료");
     }
 
 }
