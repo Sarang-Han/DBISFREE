08cd79602b6b4d4334f7a789529e489ee64bfc3e
minseokim <meanwertk@gmail.com>
2024-05-26 14:34:08 +0900

    주석 수정

--- src/java/DB2024TEAM07_RatingDAO.java
+++ src/java/DB2024TEAM07_RatingDAO.java
@@ -19,8 +19,9 @@ public class DB2024TEAM07_RatingDAO{
         this.conn = Database.getInstance().getConnection();
     }
 
+    //가게에 대한 리뷰 등록 -----------------------------------------------------------------------
     //새 리뷰 작성 시 무조건 호출되어야 함
-    //이걸 서비스 트랜잭션 구현할지 아님 DAO에서 해결볼지는 고민해봐야 할 것 같아요
+    //이걸 서비스 단에서 트랜잭션 구현할지 아님 DAO에서 해결볼지는 고민해봐야 할 것 같아요
     //개인적으로는 위에서 처리하는 게 더 깔끔할 것 같다고 생각은 합니다
     public int add(int review_id, int res_id){
         String Q = "INSERT INTO DB2024_Rating VALUES (?, ?)";
@@ -35,7 +36,7 @@ public class DB2024TEAM07_RatingDAO{
         return -2;  //error
     }
 
-    //레스토랑 평점 반환 기능
+    //레스토랑 평점 반환 기능-----------------------------------------------------------------------
     //DB2024_Review 테이블의 투플이 추가/삭제되거나, rating 속성 수정이 이뤄졌을 때 호출되어야 한다.
     //테이블 간 변화 적용이 필요한 부분이므로 트랜잭션 필수
     public float getAvg(int res_id){
