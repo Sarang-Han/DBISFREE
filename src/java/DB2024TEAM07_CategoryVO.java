d0ed1f3a78de7980d9fdfe196782aac903ce59e3
s2eojeong <s2eojeong@gmail.com>
2024-05-28 02:54:29 +0900

    CategoryVO 주석 추가

--- src/java/DB2024TEAM07_CategoryVO.java
+++ src/java/DB2024TEAM07_CategoryVO.java
@@ -4,11 +4,13 @@ public class DB2024TEAM07_CategoryVO {
     private String cuisine_type;
     private String res_name;
 
+    // 생성자
     public DB2024TEAM07_CategoryVO(String cuisine_type, String res_name) {
         this.cuisine_type = cuisine_type;
         this.res_name = res_name;
     }
 
+    // getter
     public String getCuisine_type() {
         return cuisine_type;
     }
