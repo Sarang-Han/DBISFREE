2399cb2464a4dd75210c749120017ce64b71df63
chacha <chacha09@ewhain.net>
2024-06-02 18:31:55 +0900

    update review menu

--- src/com/app/DB2024TEAM07_AdminMain.java
+++ src/com/app/DB2024TEAM07_AdminMain.java
@@ -2,6 +2,7 @@ package com.app;
 
 import com.manager.DB2024TEAM07_MenuManager;
 import com.manager.DB2024TEAM07_RestaurantManager;
+import com.manager.DB2024TEAM07_ReviewManager;
 
 import java.util.Scanner;
 
@@ -22,7 +23,17 @@ public class DB2024TEAM07_AdminMain {
             System.out.println("6. Update Menu");
             System.out.println("7. Search Menu");
             System.out.println("8. Delete Menu");
-            System.out.println("\n9. Exit");
+            System.out.println("\n========= Review =========");
+            System.out.println("9. Add Review");
+            System.out.println("10. Update Review");
+            System.out.println("11. Get Review Count");
+            System.out.println("12. Get Reviews");
+            System.out.println("13. Get User Review Count");
+            System.out.println("14. Get User Reviews");
+            System.out.println("15. Get Restaurant Review Count");
+            System.out.println("16. Get Restaurant Reviews");
+            System.out.println("17. Delete Review");
+            System.out.println("\n18. Exit");
             System.out.print("\nChoose an option: ");
             int choice = scanner.nextInt();
             scanner.nextLine();
@@ -53,6 +64,33 @@ public class DB2024TEAM07_AdminMain {
                     DB2024TEAM07_MenuManager.deleteMenu(scanner);
                     break;
                 case 9:
+                    DB2024TEAM07_ReviewManager.addReview(scanner);
+                    break;
+                case 10:
+                    DB2024TEAM07_ReviewManager.updateReview(scanner);
+                    break;
+                case 11:
+                    DB2024TEAM07_ReviewManager.getCount();
+                    break;
+                case 12:
+                    DB2024TEAM07_ReviewManager.getReview(scanner);
+                    break;
+                case 13:
+                    DB2024TEAM07_ReviewManager.getUserCount(scanner);
+                    break;
+                case 14:
+                    DB2024TEAM07_ReviewManager.getUserReview(scanner);
+                    break;
+                case 15:
+                    DB2024TEAM07_ReviewManager.getResCount(scanner);
+                    break;
+                case 16:
+                    DB2024TEAM07_ReviewManager.getResReview(scanner);
+                    break;
+                case 17:
+                    DB2024TEAM07_ReviewManager.deleteReview(scanner);
+                    break;
+                case 18:
                     running = false;
                     break;
                 default:
@@ -61,6 +99,7 @@ public class DB2024TEAM07_AdminMain {
         }
         scanner.close();
     }
+
 
     public static void search_restaurant_options(Scanner scanner){
         while (true) {
