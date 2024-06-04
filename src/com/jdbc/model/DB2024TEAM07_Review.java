6eca2cba737cb227a6785d86a18644dd94968845
minseokim <meanwertk@gmail.com>
2024-06-04 19:17:05 +0900

    테이블(DB2024_Review) 변경사항 관련 수정

--- src/com/jdbc/model/DB2024TEAM07_Review.java
+++ src/com/jdbc/model/DB2024TEAM07_Review.java
@@ -4,15 +4,13 @@ public class DB2024TEAM07_Review{
     //review_id, user_id, menu_id, rating, review_content
     private int review_id;
     private String user_id;
-    private String menu_id;
     private int rating;
     private String review_content;
 
     public DB2024TEAM07_Review(){}
-    public DB2024TEAM07_Review(int review_id, String user_id, String menu_id, int rating, String review_content) {
+    public DB2024TEAM07_Review(int review_id, String user_id, int rating, String review_content) {
         this.review_id = review_id;
         this.user_id = user_id;
-        this.menu_id = menu_id;
         this.rating = rating;
         this.review_content = review_content;
     }
@@ -31,14 +29,6 @@ public class DB2024TEAM07_Review{
 
     public void setUser_id(String user_id) {
         this.user_id = user_id;
-    }
-
-    public String getMenu_id() {
-        return menu_id;
-    }
-
-    public void setmMenu_id(String menu_id) {
-        this.menu_id = menu_id;
     }
 
     public int getRating() {
