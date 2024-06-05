31d7abccd80e73bf8f83d070d473b0734abd9161
chacha <chacha09@ewhain.net>
2024-06-06 04:05:56 +0900

    DB2024_Review_Menu_Res_Mapping 테이블 추가

--- sql/create.sql
+++ sql/create.sql
@@ -91,6 +91,18 @@ CREATE TABLE DB2024_Rating (
    FOREIGN KEY(res_id) REFERENCES DB2024_Restaurant(res_id) ON DELETE CASCADE
 );
 
+ --  DB2024_Review_Menu_Res_Mapping : addReview를 위한 매핑 테이블
+CREATE TABLE DB2024_Review_Menu_Res_Mapping (
+    mapping_id INT AUTO_INCREMENT PRIMARY KEY,
+    review_id INT,
+    menu_id INT,
+    res_id INT,
+    FOREIGN KEY (review_id) REFERENCES DB2024_Review(review_id) ON DELETE CASCADE,
+    FOREIGN KEY (menu_id) REFERENCES DB2024_Menu(menu_id) ON DELETE CASCADE,
+    FOREIGN KEY (res_id) REFERENCES DB2024_Restaurant(res_id) ON DELETE CASCADE
+);
+
+
 -- 릴레이션 확인 -----------------------------------------------------------------------
 /*
 SHOW TABLES;
@@ -410,14 +422,5 @@ CREATE INDEX DB2024_idx_Menu
 CREATE INDEX DB2024_idx_Restaurant
     ON DB2024_Restaurant(cuisine_type);
 
-CREATE TABLE DB2024_Review_Menu_Res_Mapping (
-    mapping_id INT AUTO_INCREMENT PRIMARY KEY,
-    review_id INT,
-    menu_id INT,
-    res_id INT,
-    FOREIGN KEY (review_id) REFERENCES DB2024_Review(review_id) ON DELETE CASCADE,
-    FOREIGN KEY (menu_id) REFERENCES DB2024_Menu(menu_id) ON DELETE CASCADE,
-    FOREIGN KEY (res_id) REFERENCES DB2024_Restaurant(res_id) ON DELETE CASCADE
-);
 -- 테이블 삭제 (맨 윗줄 코드로 대체) -----------------------------------------------------------------------
 -- DROP DATABASE DB2024TEAM07;
